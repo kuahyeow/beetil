4edab195a652e89581946bc393c5f7f4009490c2
Thong Kuah <kuahyeow@gmail.com>
2013-05-26 15:58:28 +1200

    Finish up this phase of CLI work, namely allow reading from ~/.beetil file
    - Add help message, and install instructions about ~/.beetil file

--- lib/beetil/runner.rb
+++ lib/beetil/runner.rb
@@ -1,6 +1,7 @@
 require 'beetil'
 require 'beetil/commands'
 require 'optparse'
+require 'shellwords'
 
 module Beetil
   class Runner
@@ -12,8 +13,6 @@ module Beetil
 
     def initialize(args)
       @options = {}
-      # FIXME extract api_token from options file
-      # extract_options_file!
       extract_arguments!(args)
       configure_beetil!
       Beetil::Commands::Command.run!(options[:command], args)
@@ -21,10 +20,12 @@ module Beetil
 
     protected
     def extract_arguments!(args)
-      options[:command] = args.shift
+      option_file_args = parse_options_file!
+      option_parser.parse!(option_file_args)
       option_parser.parse!(args)
-      print_out_command_list if options[:command].nil? || !Beetil::Commands::Command.available_commands.keys.include?(options[:command])
+      options[:command] = args.shift unless options[:command]
 
+      print_out_command_list(true) if options[:command].nil? || !Beetil::Commands::Command.available_commands.keys.include?(options[:command])
       error_out("Missing token") if options[:token].nil?
     end
 
@@ -36,10 +37,41 @@ module Beetil
 
     def option_parser
       @option_parser = OptionParser.new do |opts|
-        opts.on("-t", "--token TOKEN", "API Token") do |t|
-          options[:token] = t
+        opts.banner = "Usage: beetil command [options] [arguments]"
+
+        opts.on("--help", "Prints out this message") do
+          puts "Install: Make sure to confirm your API token from your user details."
+          puts "         All options should be readable from ~/.beetil if you so wish."
+          puts
+
+          puts opts
+          exit
+        end
+
+        opts.on("-h", "Help") do
+          puts opts
+          exit
+        end
+
+        opts.on("-l", "--list-comands", "List of commands available") do
+          print_out_command_list
+          exit
+        end
+
+        opts.on("-t", "--token TOKEN", "API Token to use") do |token|
+          options[:token] = token
+        end
+
+        opts.on("-e", "--execute COMMAND", "Command to execute") do |command|
+          options[:command] = command
         end
       end
+    end
+
+    def parse_options_file!
+      path = global_options_file
+      return [] unless path && File.exist?(path)
+      File.read(global_options_file).split(/\n+/).map {|l| Shellwords.shellwords(l) }.flatten
     end
 
     def error_out(message)
@@ -48,8 +80,13 @@ module Beetil
     end
 
     private
-    def print_out_command_list
-      message = "Specify a command. Perhaps you meant: \n"
+    def global_options_file
+      File.join(File.expand_path("~"), ".beetil")
+    end
+
+    def print_out_command_list(error = false)
+      message = "List of commands: \n"
+      message = "Specify a command. Perhaps you meant:\n" if error
       message << Beetil::Commands::Command.available_commands.map do |trigger, (klass, description)|
         "  #{trigger} - #{description}"
       end.join("\n")
